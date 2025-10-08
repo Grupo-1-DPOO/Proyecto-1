c82629f56def1f53e09d8987ed1325ce14bacf33
sramosr2 <s.ramosr2@uniandes.edu.co>
2025-10-08 15:10:37 -0500

    CAMBIO

--- src/main/java/modelo/Localidad.java
+++ src/main/java/modelo/Localidad.java
@@ -1,5 +1,38 @@
 package modelo;
 
 public class Localidad {
+	private final String nombre; 
+	private final int capacidad;
+	private final double porcentajeAumentoCosto;
+	private final Venue venue;
+	
+	public Localidad(String nombre, int capacidad, double porcentajeAumentoCosto, Venue venue) {
+	
+	this.nombre = nombre;
+	this.capacidad = capacidad;
+	this.porcentajeAumentoCosto = porcentajeAumentoCosto;
+	this.venue = venue;
+	
+	
+		
+	}
+	//GETTERS
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public int getCapacidad() {
+		return capacidad;
+	}
+
+	public double getPorcentajeAumentoCosto() {
+		return porcentajeAumentoCosto;
+	}
+
+	public Venue getVenue() {
+		return venue;
+	}
+
+	
 }
